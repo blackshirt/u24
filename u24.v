f037660515cceb04e9faf556fb5f4a9e7d682494
blackshirt <blackshirt@users.noreply.github.com>
2023-08-03 19:39:04 +0700

    Update u24.v

--- u24.v
+++ u24.v
@@ -18,19 +18,32 @@ pub fn (mut u Uint24) set_endian(big bool) {
 	u.big = big
 }
 	
-// bytes serializes underlying Uint24 data to ordinary bytes array.
+// bytes serializes underlying Uint24 data to ordinary bytes array as is.
 pub fn (v Uint24) bytes() []u8 {
 	return v.b[..]
 }
 
-// from_u32 creates Uint24 from u32
+// from_u32 creates Uint24 from u32 (in big endian form)
+// its an alias for from_big_endian_u32
 [direct_array_access; inline]
 pub fn from_u32(val u32) !Uint24 {
-	mut v := Uint24{}
+	return from_big_endian_u32(val)!
+}
+
+[direct_array_access; inline]
+pub fn from_big_endian_u32(val u32) !Uint24 {
+	mut v := Uint24{big: true}
 	v.set(val)!
 	return v
 }
-
+	
+[direct_array_access; inline]
+pub fn from_little_endian_u32(val u32) !Uint24 {
+	mut v := Uint24{big: false}
+	v.set(val)!
+	return v
+}
+	
 // from_int creates Uint24 from integer
 [direct_array_access; inline]
 pub fn from_int(val int) !Uint24 {
@@ -51,7 +64,8 @@ pub fn from_bytes(b []u8) !Uint24 {
 // to_u32 represents Uint24 to u32 value
 [direct_array_access; inline]
 pub fn (v Uint24) to_u32() !u32 {
-	val := u32(v.b[2]) | u32(v.b[1]) << u8(8) | u32(v.b[0]) << 16
+	val := if v.big { u32(v.b[2]) | u32(v.b[1]) << u8(8) | u32(v.b[0]) << 16 ) }
+		else {u32(v.b[0]) | u32(v.b[1]) << u8(8) | u32(v.b[2]) << 16 ) }
 	if val > u24.max_u24 {
 		return error('val Uint24 overflow the limit')
 	}
