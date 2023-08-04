242904c671445c0eb919c3bc23908b5c3c37391a
blackshirt <blackshirtmuslim@yahoo.com>
2023-08-04 07:30:15 +0700

    update typo

--- u24.v
+++ u24.v
@@ -1,7 +1,5 @@
 module u24
 
-import math
-
 // limit value of 24 bit opaque.
 pub const (
 	max_u24 = 1 << 24 - 1
@@ -17,7 +15,7 @@ mut:
 pub fn (mut u Uint24) set_endian(big bool) {
 	u.big = big
 }
-	
+
 // bytes serializes underlying Uint24 data to ordinary bytes array as is.
 pub fn (v Uint24) bytes() []u8 {
 	return v.b[..]
@@ -32,25 +30,29 @@ pub fn from_u32(val u32) !Uint24 {
 
 [direct_array_access; inline]
 pub fn from_big_endian_u32(val u32) !Uint24 {
-	mut v := Uint24{big: true}
+	mut v := Uint24{
+		big: true
+	}
 	v.set(val)!
 	return v
 }
-	
+
 [direct_array_access; inline]
 pub fn from_little_endian_u32(val u32) !Uint24 {
-	mut v := Uint24{big: false}
+	mut v := Uint24{
+		big: false
+	}
 	v.set(val)!
 	return v
 }
-	
+
 // from_int is an alias for `from_big_endian_int`. Its creates Uint24 from integer
 // treated as big endian, for other option, see `from_little_endian_int`
 [direct_array_access; inline]
 pub fn from_int(val int) !Uint24 {
 	return from_big_endian_u32(u32(val))!
 }
-		
+
 // from_big_endian_int creates Uint24 from big endian integer
 [direct_array_access; inline]
 pub fn from_big_endian_int(val int) !Uint24 {
@@ -70,6 +72,7 @@ pub fn from_little_endian_int(val int) !Uint24 {
 	res := from_little_endian_u32(u32(val))!
 	return res
 }
+
 // from_bytes creates Uint24 from bytes array.
 // its an alias for `from_big_endian_bytes`, for little endian
 // see `from_little_endian_bytes`
@@ -79,7 +82,7 @@ pub fn from_bytes(b []u8) !Uint24 {
 }
 
 // from_big_endian_bytes interpretes bytes as big endian.
-[direct_array_access; inline]	
+[direct_array_access; inline]
 pub fn from_big_endian_bytes(b []u8) !Uint24 {
 	val := u32_from_bytes(b, true)!
 	return from_big_endian_u32(val)
@@ -91,12 +94,16 @@ pub fn from_little_endian_bytes(b []u8) !Uint24 {
 	val := u32_from_bytes(b, false)!
 	return from_little_endian_u32(val)
 }
-		
+
 // to_u32 represents Uint24 to u32 value
 [direct_array_access; inline]
 pub fn (v Uint24) to_u32() !u32 {
-	val := if v.big { u32(v.b[2]) | u32(v.b[1]) << u8(8) | u32(v.b[0]) << 16 ) }
-		else {u32(v.b[0]) | u32(v.b[1]) << u8(8) | u32(v.b[2]) << 16 ) }
+	mut val := u32(0)
+	if v.big {
+		val = u32(v.b[2]) | u32(v.b[1]) << u8(8) | u32(v.b[0]) << 16
+	} else {
+		val = u32(v.b[2]) << 16 |  u32(v.b[1]) << u8(8) | u32(v.b[0]) 
+	}
 	// ensure result does not exceed boundary
 	if val > u24.max_u24 {
 		return error('val Uint24 overflow the limit')
@@ -138,7 +145,11 @@ fn u32_from_bytes(b []u8, big bool) !u32 {
 	if b.len != 3 {
 		return error('need 3 bytes to represent uint2')
 	}
-	val := if big { u32(b[2]) | (u32(b[1]) << u32(8)) | (u32(b[0]) << u32(16)) } else { u32(b[0]) | (u32(b[1]) << u32(8)) | (u32(b[2]) << u32(16)) }
+	val := if big {
+		u32(b[2]) | (u32(b[1]) << u32(8)) | (u32(b[0]) << u32(16))
+	} else {
+		u32(b[0]) | (u32(b[1]) << u32(8)) | (u32(b[2]) << u32(16))
+	}
 	// make sure if val does not exceed uint24 limit
 	if val > u24.max_u24 {
 		return error('val returned exceed limit')
--- u24_test.v
+++ u24_test.v
@@ -6,6 +6,7 @@ fn test_uint24_to_u32() {
 	// WARN: its big loop, long to complete
 	for i := 0; i < 1000; i++ {
 		val := rand.u32_in_range(0, max_u24)!
+
 		u := from_u32(val)!
 
 		r := u.to_u32()!
